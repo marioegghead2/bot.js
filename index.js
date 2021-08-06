87856029b83ab37908c123f8f6db09794b02721e
marioegghead2 <82485570+marioegghead2@users.noreply.github.com>
2021-08-06 16:43:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-# sorry i leaved this blank ;)
+'use strict';
+
+const WebSocket = require('./lib/websocket');
+
+WebSocket.createWebSocketStream = require('./lib/stream');
+WebSocket.Server = require('./lib/websocket-server');
+WebSocket.Receiver = require('./lib/receiver');
+WebSocket.Sender = require('./lib/sender');
+
+module.exports = WebSocket;
